054ae269d3d7b32ef642694d3205cf11daf9d2e1
Kim Yeseul <yesl00k624@gmail.com>
2021-07-23 15:08:03 +0900

    modify theme

---
+++ config.js
@@ -0,0 +1,50 @@
+"use strict";
+
+module.exports = {
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Blog by yeseul",
+  subtitle: "Front end developer",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "UA-73379983-2",
+  useKatex: false,
+  menu: [
+    {
+      label: "Articles",
+      path: "/",
+    },
+    {
+      label: "About me",
+      path: "/pages/about",
+    },
+    {
+      label: "Contact me",
+      path: "/pages/contacts",
+    },
+  ],
+  author: {
+    name: "John Doe",
+    photo: "/photo.jpg",
+    bio: "Front end developer",
+    contacts: {
+      email: "",
+      facebook: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "#",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+      medium: "",
+    },
+  },
+};
